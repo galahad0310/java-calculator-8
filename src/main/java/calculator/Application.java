7c470cf27cd629e4655103ac9ed702483f8783da
jcm <galahad000310@gmail.com>
2025-10-18 12:37:44 +0900

    feat: 기본 구분자를 사용한 덧셈 기능 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,26 @@
 package calculator;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String str = input();
+        long sum = calculator(str);
+        System.out.println("결과 : " + sum);
+    }
+
+    private static long calculator(String str){
+        if(str == null || str.isEmpty()) return 0;
+        long sum = 0;
+
+        for(String num : str.split("[,:]")){
+            sum += Long.parseLong(num);
+        }
+
+        return sum;
+    }
+
+    private static String input(){
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        return Console.readLine();
     }
 }
